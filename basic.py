961280f99dce6f2b1bec6e7792a3d6fdb593bde5
Nis9841 <nischal.rimal01@gmail.com>
2017-11-28 10:43:01 +0545

    python assignment

---
+++ basic.py
@@ -0,0 +1,18 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> p=6
+>>> n=8
+>>> (p+n)
+14
+>>> x='lap
+SyntaxError: EOL while scanning string literal
+>>> x='lap'
+>>> y='top'
+>>> print(x+y)
+laptop
+>>> x*5
+'laplaplaplaplap'
+>>> p*10
+60
+
+>>> 
